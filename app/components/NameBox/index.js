fbd8a586791296c4a2d0dda6feab6a9439ae0034
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-07-19 21:33:37 -0400

    Create component

---
+++ app/components/NameBox/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * NameBox
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class NameBox extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+NameBox.propTypes = {};
+
+export default NameBox;
