37fd6e2c0643cc371db9e4dc22501dfe5f625c76
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-07-19 22:22:16 -0400

    Create Component

---
+++ app/components/DropDown/index.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * DropDown
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class DropDown extends React.PureComponent {
+  render() {
+    return <div />;
+  }
+}
+
+DropDown.propTypes = {};
+
+export default DropDown;
