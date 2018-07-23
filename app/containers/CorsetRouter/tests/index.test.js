539cc37e6a555c0f75003637a1acfc6425c9cad2
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-07-23 19:42:30 -0400

    Create toplevel Corset Router - this will contain all corset routes

---
+++ app/containers/CorsetRouter/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import { CorsetRouter } from '../index';
+
+describe('<CorsetRouter />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
