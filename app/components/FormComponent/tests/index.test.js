3e024ec96bf9413a51d7fa4d53331f1734fb5e9c
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-07-26 20:38:41 -0400

    Improve Tests

--- app/components/FormComponent/tests/index.test.js
+++ app/components/FormComponent/tests/index.test.js
@@ -3,12 +3,18 @@ import renderer from 'react-test-renderer';
 
 import FormComponent from '../index';
 
-describe('<NameBox />', () => {
+describe('<FormComponent />', () => {
   const onSubmit = () => 1;
 
   it('Renders a FormComponent', () => {
     const tree = renderer
-      .create(<FormComponent onSubmit={onSubmit} />)
+      .create(
+        <FormComponent onSubmit={onSubmit}>
+          <label htmlFor="test">
+            <input type="text" id="test" />
+          </label>
+        </FormComponent>,
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
