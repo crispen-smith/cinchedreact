77d924a5afcb56c496d737670c56bbe1877d5c14
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-08-04 16:27:20 -0400

    Improve functionality

--- app/components/CorsetThumbnailComponent/index.js
+++ app/components/CorsetThumbnailComponent/index.js
@@ -10,34 +10,61 @@ import styled from 'styled-components';
 
 const CTC = styled.div`
   border-radius: 3px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
+  transition: 0.5s;
+  width: 20%;
+  margin: 0.5rem 0.5%;
+  flex: 0 1 auto;
+  align-self: flex-start;
+  border: 1px solid rgba(0, 0, 0, 0);
 
-  &:focus {
-    background-color: rgba(0, 0, 0, 0.1);
-    transform: scale(1.1) transformX(1.1);
-    border: 1px solid rgba(0, 0, 0, 0.2);
+  &:hover {
+    background-color: #ffffff;
+    transform: scale(1.1) translateX(10px);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
   }
 
   text-align: center;
-  flex-basis: 1;
+  flex: 0 1 auto;
   margin: 1rem;
 
   a {
     border: none;
     outline: none;
+    cursor: pointer;
+    text-decoration: none;
 
     &:focus {
       outline: none;
     }
+  }
+
+  img {
+    display: block;
+    width: 90%;
+    margin: 0 auto;
+    border-radius: 10px;
+  }
+
+  h2 {
+    font-family: 'Monsieur La Doulaise', cursive;
+    font-size: 2rem;
+    padding: none;
+    margin: none;
   }
 `;
 
 function CorsetThumbnailComponent(props) {
   return (
     <CTC>
-      <a href={`corsets/${props.CorsetName}`}>
-        <span>{props.CorsetName}</span>
-        <img alt={props.CorsetAlt} src={props.CorsetThumbnailSource} />
+      <a href={`/corsets/${props.CorsetName}`}>
+        <h2>{props.CorsetName}</h2>
+        <img
+          alt={props.CorsetAlt}
+          src={`http://res.cloudinary.com/www-cinchedtight-com/image/upload/v1533177121/${
+            props.CorsetThumbnailSource
+          }`}
+        />
         <div className="summary">{props.Summary}</div>
       </a>
     </CTC>
--- app/components/CorsetThumbnailComponent/tests/index.test.js
+++ app/components/CorsetThumbnailComponent/tests/index.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import CorsetThumbnailComponent from '../index';
 
 describe('<CorsetThumbnailComponent />', () => {
-  it('Renders a BrandBox', () => {
+  it('Renders a Corset Snapshot', () => {
     const tree = renderer
       .create(
         <CorsetThumbnailComponent
@@ -18,8 +18,3 @@ describe('<CorsetThumbnailComponent />', () => {
     expect(tree).toMatchSnapshot();
   });
 });
-
-// CorsetName: PropTypes.string.isRequired,
-// CorsetAlt: PropTypes.string.isRequired,
-// CorsetThumbnailSource: PropTypes.string.isRequired,
-// Summary: PropTypes.string.isRequired,
