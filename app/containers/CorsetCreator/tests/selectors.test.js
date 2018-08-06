337be8ed73d08fe4d252e6e18014574f673b10dd
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-08-05 20:23:55 -0400

    Remove redundant selectors and test While this breaks the 1 folder - 1 branch rule there's enough overlap between the branches that this seemed the best call.

--- app/containers/CorsetCreator/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectCorsetCreatorDomain } from '../selectors';
-
-describe('selectCorsetCreatorDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
