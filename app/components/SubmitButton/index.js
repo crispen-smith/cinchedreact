56aa6665596f312ccee0b379092d86aee0545a9e
Crispen <crispen@Crispens-MacBook-Pro.local>
2018-08-14 23:13:05 -0400

    Ensure that disabled prop is passed through to final styled input.

--- app/components/SubmitButton/index.js
+++ app/components/SubmitButton/index.js
@@ -9,19 +9,33 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import colors from '../../resources/colors';
 
-const SB = styled.input`
+const SB = styled.input.attrs({
+  'data-cursor': props => props['data-cursor'] || 'not-allowed',
+})`
   font-size: 2rem;
   margin: 1rem auto;
   border-radius: 25%;
   background-color: ${colors.pink};
+  cursor: ${props => props['data-cursor']};
 `;
 
 function SubmitButton(props) {
-  return <SB type="submit" value={props.label} />;
+  return (
+    <SB
+      type="submit"
+      value={props.label}
+      disabled={!props.enabled}
+      onClick={props.onClick}
+      data-cursor={props.dataCursor}
+    />
+  );
 }
 
 SubmitButton.propTypes = {
   label: PropTypes.string.isRequired,
+  enabled: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  dataCursor: PropTypes.string.isRequired,
 };
 
 export default SubmitButton;
--- app/components/SubmitButton/tests/index.test.js
+++ app/components/SubmitButton/tests/index.test.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import SubmitButton from '../index';
 
 describe('<SubmitButton />', () => {
-  it('Renders a BrandBox', () => {
-    const tree = renderer.create(<SubmitButton label="test" />).toJSON();
+  it('Renders a disabled SubmitButton when the props include enabled-false', () => {
+    const element = (
+      <SubmitButton
+        label="test"
+        enabled={false}
+        onClick={() => true}
+        dataCursor="pointer"
+      />
+    );
+    const button = renderer.create(element).toJSON();
+    expect(button).toMatchSnapshot();
+
+    const sb = mount(element);
+    const input = sb.find('input').at(0);
+
+    expect(input.props().disabled).toEqual(true);
+  });
+
+  it('Renders a disabled SubmitButton when the props include enabled-true', () => {
+    const element = (
+      <SubmitButton
+        label="test"
+        enabled
+        onClick={() => true}
+        dataCursor="pointer"
+      />
+    );
+    const tree = renderer.create(element).toJSON();
     expect(tree).toMatchSnapshot();
+
+    const sb = mount(element);
+    const input = sb.find('input').at(0);
+
+    expect(input.props().disabled).toEqual(false);
   });
 });
