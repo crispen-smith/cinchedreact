7d611da3360ddaec42f2ccde70fafbde784bfce4
Crispen <crispen@Crispens-MacBook-Pro.local>
2019-01-28 21:38:23 -0500

    Improve Reset Action tests

--- app/containers/CorsetReset/tests/actions.test.js
+++ app/containers/CorsetReset/tests/actions.test.js
@@ -1,7 +1,7 @@
 import { defaultAction, resetAction } from '../actions';
 import CORSET_RESET_ACTION_TYPES from '../constants';
 
-describe('CorsetGallery actions', () => {
+describe('CorsetReset actions', () => {
   describe('Default Action', () => {
     it('has a type of DEFAULT_ACTION', () => {
       const expected = {
@@ -9,7 +9,9 @@ describe('CorsetGallery actions', () => {
       };
       expect(defaultAction()).toEqual(expected);
     });
+  });
 
+  describe('Reset Action', () => {
     it('has a type of RESET_ACTION', () => {
       const expected = {
         type: CORSET_RESET_ACTION_TYPES.resetAction,
